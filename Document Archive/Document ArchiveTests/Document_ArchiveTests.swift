4c7f7422b73d2d97959fdbb2aafba7f1d19b8bf8
lapsedPacifist <don_willems@me.com>
2015-06-19 17:29:58 +0200

    Project creation
    The project was created in XCode using the Swift language and Core Data.

---
+++ Document Archive/Document ArchiveTests/Document_ArchiveTests.swift
@@ -0,0 +1,36 @@
+//
+//  Document_ArchiveTests.swift
+//  Document ArchiveTests
+//
+//  Created by Don Willems on 19/06/15.
+//  Copyright (c) 2015 lapsedpacifist. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+
+class Document_ArchiveTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
