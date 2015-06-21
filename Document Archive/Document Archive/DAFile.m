d1555de89658a0280cb0c3830335a1599e480442
lapsedPacifist <don_willems@me.com>
2015-06-21 19:58:56 +0200

    Created initial data model
    Created data model in Core Data datamodel file and created the corresponding NSManagedObject subclasses.
    At this moment two classes exist: Tag and File.

---
+++ Document Archive/Document Archive/DAFile.m
@@ -0,0 +1,22 @@
+//
+//  DAFile.m
+//  Document Archive
+//
+//  Created by Don Willems on 21/06/15.
+//  Copyright (c) 2015 lapsedpacifist. All rights reserved.
+//
+
+#import "DAFile.h"
+#import "DATag.h"
+
+
+@implementation DAFile
+
+@dynamic path;
+@dynamic creationDate;
+@dynamic modificationDate;
+@dynamic source;
+@dynamic title;
+@dynamic tags;
+
+@end
---
+++ Document Archive/Document Archive/DATag.m
@@ -0,0 +1,22 @@
+//
+//  DATag.m
+//  Document Archive
+//
+//  Created by Don Willems on 21/06/15.
+//  Copyright (c) 2015 lapsedpacifist. All rights reserved.
+//
+
+#import "DATag.h"
+#import "DAFile.h"
+#import "DATag.h"
+
+
+@implementation DATag
+
+@dynamic string;
+@dynamic broader;
+@dynamic narrower;
+@dynamic sameAs;
+@dynamic files;
+
+@end
