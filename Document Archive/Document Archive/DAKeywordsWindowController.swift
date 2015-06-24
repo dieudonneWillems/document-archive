e0bf8c71d7756cb9357a6840e713d086125f1f1f
lapsedPacifist <don_willems@me.com>
2015-06-24 18:28:25 +0200

    Created Keywords window
    Created keywords window nib and controller class.
    Created action to pop up the window using a button action.

---
+++ Document Archive/Document Archive/DAKeywordsWindowController.swift
@@ -0,0 +1,19 @@
+//
+//  DAKeywordsWindowController.swift
+//  Document Archive
+//
+//  Created by Don Willems on 24/06/15.
+//  Copyright (c) 2015 lapsedpacifist. All rights reserved.
+//
+
+import Cocoa
+
+class DAKeywordsWindowController: NSWindowController {
+
+    override func windowDidLoad() {
+        super.windowDidLoad()
+
+        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+    }
+    
+}
