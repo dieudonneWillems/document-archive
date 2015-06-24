601c79fd0f894fb5a23ad7fe0c73915624277011
lapsedPacifist <don_willems@me.com>
2015-06-25 00:47:33 +0200

    Made Keyword Window vibrant

--- Document Archive/Document Archive/DAKeywordsWindowController.swift
+++ Document Archive/Document Archive/DAKeywordsWindowController.swift
@@ -12,7 +12,9 @@ class DAKeywordsWindowController: NSWindowController {
 
     override func windowDidLoad() {
         super.windowDidLoad()
-
+        self.window!.titleVisibility = NSWindowTitleVisibility.Hidden
+        self.window!.titlebarAppearsTransparent = true
+        self.window!.styleMask |= NSFullSizeContentViewWindowMask
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
     
