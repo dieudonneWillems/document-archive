ee0de526f9d0f8d5ce80dbf78e3a69fd65cbebef
lapsedPacifist <don_willems@me.com>
2015-06-25 09:12:49 +0200

    Connected datasource methods
    Connected data source methods of the outlineview showing the tags to the window controller.
    
    Changed layout of the keyword window.

--- Document Archive/Document Archive/DAKeywordsWindowController.swift
+++ Document Archive/Document Archive/DAKeywordsWindowController.swift
@@ -9,6 +9,8 @@
 import Cocoa
 
 class DAKeywordsWindowController: NSWindowController {
+    
+    @IBOutlet weak var outlineView : NSOutlineView!
 
     override func windowDidLoad() {
         super.windowDidLoad()
@@ -16,6 +18,36 @@ class DAKeywordsWindowController: NSWindowController {
         self.window!.titlebarAppearsTransparent = true
         self.window!.styleMask |= NSFullSizeContentViewWindowMask
         // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+        self.reloadData()
     }
     
+    // MARK: - Reloading data
+    
+    func reloadData() {
+        let appdelegate = NSApplication.sharedApplication().delegate as! AppDelegate
+        outlineView.reloadData()
+    }
+    
+    
+    // MARK: - Data source methods for outline view with tags
+    
+    func outlineView(outlineView: NSOutlineView, child index: Int, ofItem item: AnyObject?) -> AnyObject {
+        return "test"
+    }
+    
+    func outlineView(outlineView: NSOutlineView, isItemExpandable item: AnyObject) -> Bool {
+        return false;
+    }
+    
+    func outlineView(outlineView: NSOutlineView, numberOfChildrenOfItem item: AnyObject?) -> Int {
+        return 5;
+    }
+    
+    func outlineView(outlineView: NSOutlineView, objectValueForTableColumn tableColumn: NSTableColumn?, byItem item: AnyObject?) -> AnyObject? {
+        return item;
+    }
+    
+    func outlineView(outlineView: NSOutlineView, setObjectValue object: AnyObject?, forTableColumn tableColumn: NSTableColumn?, byItem item: AnyObject?) {
+        
+    }
 }
