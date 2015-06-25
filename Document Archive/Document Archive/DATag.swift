95ef6da87afb94368222b802be02e40f813842df
lapsedPacifist <don_willems@me.com>
2015-06-25 22:50:53 +0200

    Added access methods for derived tag sets
    Function to derive all root tags of a tag.
    Functions to test wheter a tag is a broader or narrower tag of a tag.
    Check on circularity when adding narrower or broader tags.
    Function to retrieve all files associated with a tag, both manually and automatically added (proposed) files.

--- Document Archive/Document Archive/DATag.swift
+++ Document Archive/Document Archive/DATag.swift
@@ -25,6 +25,69 @@ class DATag: NSManagedObject {
 extension DATag {
     
     /**
+    Returns an array with all broader (parent) tags that are root tags.
+    This function is recursive and returns the broader terms which do not have
+    broader terms themselves.
+    
+    :returns: All root tags.
+    */
+    func rootTags() -> [DATag] {
+        var roottags = [DATag]()
+        if broader.count == 0 {
+            roottags.append(self)
+        } else {
+            for parent in broader {
+                let broot = parent.rootTags()
+                for root in broot {
+                    if !contains(roottags, root) {
+                        roottags.append(root)
+                    }
+                }
+            }
+        }
+        return roottags
+    }
+    
+    
+    /**
+    Checks whether the specified tag is a broader tag of the receiver.
+    
+    :param: tag The tag to be tested.
+    
+    :returns: True when the tag is a broader tag, false otherwise.
+    */
+    func isBroader(tag :DATag) -> Bool {
+        for parent in broader {
+            if tag === parent {
+                return true
+            }
+            if parent.isBroader(tag) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    /**
+    Checks whether the specified tag is a narrower tag of the receiver.
+    
+    :param: tag The tag to be tested.
+    
+    :returns: True when the tag is a narrower tag, false otherwise.
+    */
+    func isNarrower(tag :DATag) -> Bool {
+        for child in narrower {
+            if tag === child {
+                return true
+            }
+            if child.isNarrower(tag) {
+                return true
+            }
+        }
+        return false
+    }
+    
+    /**
     Returns an array with all broader (parent) tags of the receiver.
     The array is sorted alphabetically.
     
@@ -41,10 +104,14 @@ extension DATag {
     The specified tag is considered to be the parent of the receiver tag.
     e.g. 'apple' has as broader tag 'fruit'.
     A narrower tag is automatically added with the receiver tag as the narrower tag.
+    The hierarchy is checked for circularity.
     
     :param: tag The parent or broader tag.
     */
     func addBroaderTag(tag:DATag) {
+        if isNarrower(tag) {
+            return
+        }
         var nset = NSMutableSet()
         nset.setSet(broader as Set<NSObject>)
         nset.addObject(tag)
@@ -81,10 +148,14 @@ extension DATag {
     The specified tag is considered to be the child of the receiver tag.
     e.g. 'fruit' has as narrower tag 'apple'.
     A broader tag is automatically added with the receiver tag as the broader tag.
+    The hierarchy is checked for circularity.
     
     :param: tag The child or narrower tag.
     */
     func addNarrowerTag(tag:DATag) {
+        if isBroader(tag) {
+            return
+        }
         var nset = NSMutableSet()
         nset.setSet(narrower as Set<NSObject>)
         nset.addObject(tag)
@@ -143,6 +214,23 @@ extension DATag {
         sameAs = nset
     }
     
+    /**
+    Returns all files, including proposed files.
+    
+    :returns: All files.
+    */
+    func allFiles() -> [DAFile] {
+        var allfiles = [DAFile]()
+        //TODO
+        let filesarray = files.allObjects as! [DAFile]
+        allfiles += filesarray
+        for pfile in proposedForFiles {
+            if !contains(allfiles, pfile as! DAFile) {
+                allfiles.append(pfile as! DAFile)
+            }
+        }
+        return allfiles
+    }
     
     /**
     Associates a file with the receiver tag.
